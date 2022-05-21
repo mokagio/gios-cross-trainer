50dbdbccb5ae49ee4132db7391260985ad4563ce
Gio Lodi <giovanni.lodi42@gmail.com>
2022-05-22 04:59:31 +1000

    "Configure" Tailwind – Just a tweak of what the tutorial said...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,17 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  content: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // TODO: enable
   theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor'
+      ....
+    }
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
