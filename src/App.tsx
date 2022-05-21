f720de05d45a99faa1a0c6e47a8c110d35f2eb34
Gio Lodi <giovanni.lodi42@gmail.com>
2022-05-22 06:45:33 +1000

    Render scrambled image for reference via `rs-visualizer`

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import getRandomSolve from 'rubiks-cross-trainer';
 import * as SRVisualizer from 'sr-visualizer';
 
 function App() {
+  // FIXME: I'm sure there's a better hook that this that doesn't require the `data-done` hack
+  useEffect(() => {
+    [...Array(8)].map(function(_, i) {
+      let id = `scramble-${i + 1}`
+      let element = document.getElementById(id)
+      // FIXME: Remove once found proper hook for "page loaded"
+      if (element?.getAttribute('data-done') == 'true') { return }
+
+      let algorithm = element?.getAttribute('data-algorithm')
+
+      SRVisualizer.cubePNG(element, { algorithm: algorithm })
+      // FIXME: Remove once found proper hook for "page loaded"
+      element?.setAttribute('data-done', 'true')
+    })
+  })
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Gio's Cross Trainer</h1>
 
       {[...Array(8)].map(function (_, i) {
-      let moves = i + 1
-      let scramble = getRandomSolve(moves)
+        let moves = i + 1
+        let scramble = getRandomSolve(moves)
+        let imageNodeId = "scramble-" + moves
 
-      return (
-      <div>
-        <p>Scramble for {moves}-moves cross</p>
-        <p>{ scramble }</p>
-      </div>
+        return (
+        <div>
+          <p>Scramble for {moves}-moves cross</p>
+          <p>{scramble}</p>
+          <p id={imageNodeId} data-algorithm={scramble}></p>
+        </div>
       )})}
 
       <div>
